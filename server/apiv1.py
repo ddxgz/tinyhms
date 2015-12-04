68705662f61e6d45f22a157e57e10030cf5d1798
ddxgz <ddxgz19880813@gmail.com>
2015-12-04 19:45:44 +0000

    added appoint post and get api without redis

--- server/apiv1.py
+++ server/apiv1.py
@@ -8,7 +8,7 @@ import functools
 
 import falcon
 
-from server import doctor
+from server import doctor, appointment
 from server.utils import logger
 
 
@@ -145,24 +145,23 @@ class DoctorListener:
 class MakeAppointmentListener:
 
     @falcon.before(max_body(64*1024))
-    def on_post(self, req, resp, user, appname):
+    def on_post(self, req, resp):
         """
-        :param req.header.username: the username, should be tenant:user when dev
+        Make an appointment in the system. The post data is in json format.
+
+        :param req.header.username: username
         :param req.header.password: password
-        :returns: a json contains all objects in disk container, and metameata
-                {"meta":{}, "objects":{"obj1": {}}}
+        :returns: a json contains the appointment's url, or other related info
         """
         resp_dict = {}
-        docker_resp = 'nothing'
         try:
             username = req.get_header('username') or 'un'
             password = req.get_header('password') or 'pw'
             # post_data = req.params.get('data')
+            # have pre-processed by JSONTranslator, post_data is a dict
             post_data = req.context['doc']
-            logger.debug('username:%s, password:%s, data:%s'
-                % (username, password, post_data))
-            logger.debug(' user:{}, appname:{}'.format(
-                user, appname))
+            # logger.debug('username:%s, password:%s, data:%s'
+            #     % (username, password, post_data))
             # logger.debug('env:%s , \nstream:%s, \ncontext:, \ninput:' % (
             #     req.env, req.stream.read()))
         except Exception as ex:
@@ -170,45 +169,88 @@ class MakeAppointmentListener:
             raise falcon.HTTPBadRequest('bad req',
                 'when read from req, please check if the req is correct.')
         try:
-
-            """
-            handle_request:
-
-            """
-            status, body = create_app(user, appname, post_data)
+            status, appointment_url = appointment.make_appointment(post_data)
 
         except Exception as ex:
-            logger.exception('error when try to handle_request, ', ex)
-            resp_dict['info'] = 'user:{} password not correct'.format(user)
-            resp.status = falcon.HTTP_400
-            resp.body = json.dumps(resp_dict)
+            logger.exception('error when Make an appointment, ', ex)
+            resp_dict['info'] = 'Error when Make an appointment {}'.format(
+                post_data['doctorid']+post_data['patientid']+post_data['datatimeslot'])
+            resp.status = falcon.HTTP_500
+            resp.body = json.dumps(resp_dict, sort_keys=True, indent=4)
         else:
-            # resp.status = status or falcon.HTTP_200
-            resp.status = falcon.HTTP_200
-            resp.body = body or json.dumps(resp_dict,
-                sort_keys=True, indent=4)
-
-    def on_get(self, req, resp):
-        logger.debug('in get for:{}'.format(req.get_header('username')))
-
-        resp.stream = ResponseStream(resp.stream)
-        service_requests = print_int(req.get_header('username'),
-                callback=resp.stream.write)
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(service_requests)
-
-
-    def on_delete(self, req, resp):
-        pass
+            if status:
+                logger.debug('make appointment ok, status positive')
+                resp_dict['info'] = 'make appointment {} success'.format(
+                    post_data['doctorid']+post_data['patientid']+post_data['datatimeslot'])
+                resp_dict['appointment_url'] = appointment_url
+                # resp.status = status or falcon.HTTP_200
+                resp.status = falcon.HTTP_200
+                resp.body = json.dumps(resp_dict,
+                    sort_keys=True, indent=4)
+            else:
+                logger.exception('return error when try to make appointment, ', ex)
+                resp_dict['errinfo'] = 'Error when make appointment {}'.format(
+                    post_data['doctorid']+post_data['patientid']+post_data['datatimeslot'])
+                resp.status = falcon.HTTP_400
+                resp.body = json.dumps(resp_dict, sort_keys=True,
+                    indent=4)
 
 
 class AppointmentListener:
 
-    @falcon.before(max_body(64*1024))
-    def on_post(self, req, resp, user, appname):
-        pass
 
-    def on_get(self, req, resp):
+    def on_get(self, req, resp, doctorid, datetimeslot, patientid):
+        """
+        Get info of a doctor in the system. The response data is in json format.
+
+        :param req.header.username: username
+        :param req.header.password: password
+        :returns: a json contains doctor's info
+                {"doctorid":'d001', "info":{"info1":''}}
+        """
+        resp_dict = {}
+        try:
+            username = req.get_header('username') or 'un'
+            password = req.get_header('password') or 'pw'
+            # logger.debug('env:%s , \nstream:%s, \ncontext:, \ninput:' % (
+            #     req.env, req.stream.read()))
+        except Exception as ex:
+            logger.error('error when try to get headers and data, ', ex)
+            raise falcon.HTTPBadRequest('bad req',
+                'when read from req, please check if the req is correct.')
+        try:
+            """
+            handle_request:
+
+            """
+            apmt_url = doctorid + '/' + datetimeslot + '/' + patientid
+            status, appointment_info = appointment.get_appointment(apmt_url)
+
+        except Exception as ex:
+            logger.exception('error when get appointment_info, ', ex)
+            resp_dict['info'] = 'Error when get appointment_info {}'.format(
+                apmt_url)
+            resp.status = falcon.HTTP_500
+            resp.body = json.dumps(resp_dict, sort_keys=True, indent=4)
+        else:
+            if status:
+                logger.debug('get ok, status positive')
+                resp_dict['info'] = 'Get appointment_info {} success'.format(
+                    apmt_url)
+                resp_dict['appointment_info'] = appointment_info
+                # resp.status = status or falcon.HTTP_200
+                resp.status = falcon.HTTP_200
+                resp.body = json.dumps(resp_dict,
+                    sort_keys=True, indent=4)
+            else:
+                logger.exception('return error when try to get appointment_info')
+                resp_dict['info'] = 'Error when get appointment_info {}'.format(
+                    apmt_url)
+                resp.status = falcon.HTTP_400
+                resp.body = json.dumps(resp_dict, sort_keys=True,
+                    indent=4)
+
+    def on_put(self, req, resp):
         pass
 
     def on_delete(self, req, resp):
---
+++ server/appointment.py
@@ -0,0 +1,65 @@
+import json
+
+from server.models import database, DoctorModel
+from server.hmsexceptions import UserNotExistException
+from server.utils import logger
+
+
+def make_appointment(post_data):
+    """
+    make_appointment in the system. The post data is in json format.
+
+    :param post_data: dict
+    :returns: a status, a str( appointment's url on success, err info on failure)
+    """
+    print(post_data)
+    try:
+        logger.debug('in make_appointment')
+
+    # check db when patient is ok
+    """        with database.atomic():
+                doctor = DoctorModel.create_by_dict(post_data)
+                logger.debug(doctor)
+                logger.debug('in database.atomic')
+        except peewee.IntegrityError:
+            logger.warning('in doctor model create except')"""
+
+        """
+        1. check if patient and doctor exist in db
+        2. check if the appointment exist in redis
+        3. make appointment if 1 and 2 ok
+        4. return if appointment exists, with reason if fail
+        """
+    except Exception as ex:
+        logger.error('Exception: ', ex)
+        # q = DoctorModel.delete().where(DoctorModel.uid==doctor)
+        # q.execute()
+        return 0, 'make_appointment failed, did not make_appointment'
+
+    else:
+        return 1, str(doctor)
+
+
+def get_appointment(appointment_url):
+    """
+    Get info of an appointment in the system.
+
+    :param appointment_url: appointment's url
+    :returns: a status, a str ( appointment's info on success, err info on failure)
+    """
+    print(appointment_url)
+    info = {}
+    try:
+        logger.debug('in get_appointment')
+        # check redis
+        # appointment = DoctorModel.get_dict(appointment_url)
+        logger.debug(appointment)
+    # except UserNotExistException:
+    #     logger.debug('in UserNotExistException')
+    #     return 0, 'get doctor failed, the required Doctor Did Not Exist'
+    except Exception as ex:
+        logger.error('Exception: ', ex)
+        return 0, 'get appointment failed'
+    else:
+        appointment_json = json.dumps(appointment)
+        return 1, appointment_json
